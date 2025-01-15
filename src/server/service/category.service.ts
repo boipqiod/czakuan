63f7b7e35fdc015b177b221370b45e0b1181b9f5
Kong <boipqiod@naver.com>
2025-01-15 09:04:22 +0900

    feat. 최근 작성글 카테고리 3개 가오기

--- src/server/service/category.service.ts
+++ src/server/service/category.service.ts
@@ -40,4 +40,29 @@ export class CategoryService {
 
     return {categoryGroups};
   }
+
+  getRecentCategories() {
+    return prisma.post.findMany({
+      where: {
+        // 제외할 조건들
+        isAnonymous: false, // 익명 게시글 제외
+        isNotice: false, // 공지사항 제외
+        deletedAt: null, // 삭제된 게시글 제외
+
+        // 카테고리가 사용 중이고 익명카테고리가 아닌 것만
+        category: {
+          isUse: true,
+          isAnonymous: false,
+        },
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+      distinct: ['categoryId'], // 서로 다른 카테고리만
+      take: 3, // 3개만 가져오기
+      select: {
+        category: true,
+      },
+    });
+  }
 }
